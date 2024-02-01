68cf850ebc2961389e285b541095bd1e04f20c9e
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-02-01 13:48:31 +0000

    Edited prompt and box design

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ function generateFact(event) {
   let topicInput = document.querySelector("#user-topic");
   let apiKey = "97ca3fd7c1847b8c2t8cf2o00a730a3a";
   let context =
-    "You are a knowledgeable AI assistant that can provide intriguing short facts. Your mission is to present a fact using basic HTML, keeping it within 6 lines. Avoid including a title or heading, and ensure adherence to user instructions.";
-  let prompt = `User instructions: Generate a short interesting fact about ${topicInput.value}`;
+    "You are a knowledgeable AI assistant that can provide intriguing short facts. Your mission is to present an interesting fact using basic HTML, keeping it within 6 lines. Avoid including a title or heading, and ensure adherence to user instructions.";
+  let prompt = `User instructions: Generate a short interesting and informative fact about ${topicInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let factElement = document.querySelector("#fact");
